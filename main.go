c84ffaa46e3b8b72e10b53a108ac5282a44e4222
engineer <engineer@engineering.com>
2020-09-04 18:12:00 -0500

    extracting html tags

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -18,20 +19,45 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		nav := strings.Split(scanner.Text(), " ")
-		ip := nav[0]
-		nNav := nav[2:]
-		fmt.Println("IP", ip, "Nav:", nNav)
 
+	for scanner.Scan() {
+		txt := strings.Split(scanner.Text(), " ")
+		ip := txt[0]
+		nav := cleanString(txt[1:])
+		fmt.Println("IP", ip, "Nav:", nav)
 		res, err := http.Get("http://" + ip + "/srm1TravelDistanceList.html")
-		defer res.Body.Close()
 		body, err := ioutil.ReadAll(res.Body)
 
 		pageContent := string(body)
-		fmt.Println("body:", pageContent, "err:", err)
+
+		r := regexp.MustCompile(`<tr.*?>(.*)</tr>`)
+
+		submatchall := r.FindAllStringSubmatch(pageContent, -1)
+
+		// fmt.Println("body:", pageContent, "err:", err)
+		// if r.MatchString(pageContent) {
+		// 	fmt.Println("body:", pageContent, "err:", err)
+		// }
+
+		for _, element := range submatchall {
+			fmt.Println(element[1], "err", err)
+		}
+
+		defer res.Body.Close()
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func cleanString(s []string) string {
+	//nav := strings.Trim(strings.Replace(strings.Join(txt[1:], " "), "[", "", -1), " ")
+	// strings.Join converts the string array to string
+	// strings.Replace removes the [] in the string
+	// strings.Trim removes the whitespace
+	n := strings.Join(s[1:], " ")
+	n = strings.Replace(n, "[", "", -1)
+	n = strings.Trim(n, " ")
+	return n
 }
